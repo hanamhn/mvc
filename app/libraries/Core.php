526bfaa70a7bcd7105f4f6d0cf07c345e108326b
hanamhn <hanamhndev@gmail.com>
2022-11-10 16:54:56 +0700

    initial mvc php

---
+++ app/libraries/Core.php
@@ -0,0 +1,40 @@
+<?php 
+/*
+* App Core Class
+* Cretes URL & loads core controller
+* URL FORMAT - /controller/method/params
+*/
+
+class Core{
+	protected $currentController = 'Pages';
+	protected $currentMethod = 'index';
+	protected $params = [];
+
+
+	public function __construct()
+	{
+		// print_r($this->getUrl());
+		$url = $this->getUrl();
+
+		// Look in controllers for first value 
+		if(file_exists('../app/controllers/'.ucwords($url[0]).'.php')){
+			// If exists, set as controller
+			$this->currentController = ucwords($url[0]);
+			// Unset 0 Index
+			unset($url[0]);
+		}
+	}
+
+	public function getUrl(){
+		if(isset($_GET['url'])){
+			$url = rtrim($_GET['url'], '/');
+			$url = filter_var($url, FILTER_SANITIZE_URL);
+			$url = explode('/', $url);
+
+			return $url;
+		}
+	}
+
+}
+
+?>
